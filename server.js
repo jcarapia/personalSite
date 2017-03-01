0db61d39fb2929e86cdc06a11d08ec25eebd530d
Jorge <jcarapia83@Gmail.com>
2017-02-28 17:49:45 -0800

    fixed server issues

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 
 // Environment Variables
 var PORT = process.env.PORT || 3000;
-//var api_key = process.env.api_key || 0;
-//var my_email = process.env.email_address || 'dev_email';
-//var domain = 'sandbox614028c0d12b4e978dcdeca5945a9a18.mailgun.org';
-//var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+var api_key = process.env.api_key;
+var my_email = process.env.email_address;
+var domain = 'sandbox614028c0d12b4e978dcdeca5945a9a18.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
  
 // Express Configuration
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,28 +25,28 @@ app.get('/resume', function(req, res) {
 	res.sendFile(path.join(__dirname + '/resources/files/jorgeCarapiaResume.pdf'));
 })
 
-// app.post('/sendMail', function(req, res) {
-// 	var name = req.body.name;
-// 	var email = req.body.email;
-// 	var message = req.body.message;
-// 	var antispam = req.body.antispam;	
+app.post('/sendMail', function(req, res) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var message = req.body.message;
+	var antispam = req.body.antispam;	
 
-// 	if(antispam === '10') {
-// 		var data = {
-// 			from: name + ' <' +email+ '>', 
-// 			to: my_email,
-// 			subject: 'New Message from JorgeCarapia.com',
-// 			text: message
-// 		};
+	if(antispam === '10') {
+		var data = {
+			from: name + ' <' +email+ '>', 
+			to: my_email,
+			subject: 'New Message from JorgeCarapia.com',
+			text: message
+		};
 
-// 		mailgun.messages().send(data, function (error, body) {
-//     	console.log(body);
-//  		});
+		mailgun.messages().send(data, function (error, body) {
+    	console.log(body);
+ 		});
 
-// 		res.redirect('/');
-// 	};
+		res.redirect('/');
+	};
 	
-// });
+});
 
 app.listen(PORT, () => {
 	console.log('The magic happens on port: ' + PORT);
